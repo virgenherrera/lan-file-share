3ecfd8c20fa0524b58e69f584d8e54d417c6fb47
Hugo Virgen <virgenherrera@gmail.com>
2022-07-14 19:23:32 -0500

    - install @nestjs/serve-static. (#11)
    - init web app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { CoreModule } from './core/core.module';
 import { LogRequestMiddleware } from './core/middleware';
 import { MultimediaModule } from './multimedia/multimedia.module';
@@ -10,6 +12,17 @@ import { QrStdoutModule } from './qr-stdout/qr-stdout.module';
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveStaticOptions: {
+        setHeaders(res) {
+          res.setHeader(
+            'Content-Security-Policy',
+            `default-src *; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; style-src	'self' 'unsafe-inline' *; img-src 'self' data: *`,
+          );
+        },
+      },
     }),
     CoreModule,
     MultimediaModule,
