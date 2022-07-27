f6db66b6ac73c2e874cb28db24d554a9152b6382
Hugo Virgen <virgenherrera@gmail.com>
2022-07-27 14:19:49 -0500

    fix: use real port on stdOut (#27)
    - provide EnvConfigService.
    - refactor AfterApplicationBootstrapService.setUrl();

--- src/qr-stdout/qr-stdout.module.ts
+++ src/qr-stdout/qr-stdout.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { EnvConfigService } from '../core/services';
 import { AfterApplicationBootstrapService } from './services/after-application-bootstrap.service';
 
 @Module({
-  providers: [AfterApplicationBootstrapService],
+  providers: [EnvConfigService, AfterApplicationBootstrapService],
 })
 export class QrStdoutModule {}
