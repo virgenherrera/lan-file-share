b11c2193a52d204502a1643b7b01c769a9c2813a
Hugo Virgen <virgenherrera@gmail.com>
2022-08-08 15:44:05 -0500

    - complete FileController UT.

--- src/multimedia/controllers/file.controller.spec.ts
+++ src/multimedia/controllers/file.controller.spec.ts
@@ -1,10 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SharedFolderServiceProvider } from '../services/__mocks__';
+import { Response } from 'express';
+import {
+  mockSharedFolderService,
+  SharedFolderServiceProvider,
+} from '../services/__mocks__';
 import { FileController } from './file.controller';
 
 describe(`UT:${FileController.name}`, () => {
   const enum should {
     createInstance = 'should create instance Properly.',
+    getSreamableFile = 'Should get Streamable File.',
+    getFolderContents = 'Should get folder contents.',
   }
 
   let controller: FileController = null;
@@ -21,5 +27,46 @@ describe(`UT:${FileController.name}`, () => {
   it(should.createInstance, () => {
     expect(controller).not.toBeNull();
     expect(controller).toBeInstanceOf(FileController);
+  });
+
+  it(should.getSreamableFile, async () => {
+    const mockFilePath = 'fake/path';
+    const response = {
+      set: jest.fn(),
+    } as any as Response;
+    const mockData = {
+      headers: { foo: 'bar' },
+      streamableFile: { foo: 'bar' },
+    };
+
+    mockSharedFolderService.getDownloadableFile = jest
+      .fn()
+      .mockResolvedValue(mockData);
+
+    const responseSetSpy = jest.spyOn(response, 'set');
+    const serviceSpy = jest.spyOn(
+      mockSharedFolderService,
+      'getDownloadableFile',
+    );
+
+    await expect(controller.getFile(mockFilePath, response)).resolves.toBe(
+      mockData.streamableFile,
+    );
+    expect(responseSetSpy).toHaveBeenCalledWith(mockData.headers);
+    expect(serviceSpy).toHaveBeenCalledWith(mockFilePath);
+  });
+
+  it(should.getFolderContents, async () => {
+    const mockFolderPath = 'fake/path';
+    const mockData = { foo: 'bar' };
+
+    mockSharedFolderService.getPathInfo = jest.fn().mockResolvedValue(mockData);
+
+    const serviceSpy = jest.spyOn(mockSharedFolderService, 'getPathInfo');
+
+    await expect(controller.getSharedFolder(mockFolderPath)).resolves.toBe(
+      mockData,
+    );
+    expect(serviceSpy).toHaveBeenCalledWith(mockFolderPath);
   });
 });
