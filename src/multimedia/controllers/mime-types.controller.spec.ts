842fc61dcb4fbe85879cba2478ccb04b04a6c6f5
Hugo Virgen <virgenherrera@gmail.com>
2022-08-08 15:48:02 -0500

    - complete MimeTypesController UT.

--- src/multimedia/controllers/mime-types.controller.spec.ts
+++ src/multimedia/controllers/mime-types.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MediaMimeTypes } from '../constants';
 import { MimeTypesController } from './mime-types.controller';
 
-describe('MimeTypesController', () => {
-  let controller: MimeTypesController;
+describe(`UT:${MimeTypesController.name}`, () => {
+  const enum should {
+    createInstance = 'should create instance Properly.',
+    getMediaMimeTypes = 'Should get MediaMimeTypes.',
+  }
 
-  beforeEach(async () => {
+  let controller: MimeTypesController = null;
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MimeTypesController],
     }).compile();
 
-    controller = module.get<MimeTypesController>(MimeTypesController);
+    controller = module.get(MimeTypesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it(should.createInstance, () => {
+    expect(controller).not.toBeNull();
+    expect(controller).toBeInstanceOf(MimeTypesController);
+  });
+
+  it(should.getMediaMimeTypes, async () => {
+    const data = { data: MediaMimeTypes };
+
+    await expect(controller.getMimeTypes()).resolves.toBe(data);
   });
 });
