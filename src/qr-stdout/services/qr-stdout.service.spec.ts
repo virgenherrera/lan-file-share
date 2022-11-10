0867cd84e2c97bd6d9554b20f9056b762ee0f87f
Hugo Virgen <virgenherrera@gmail.com>
2022-11-10 16:31:02 -0600

    * rename to QrStdoutService.

--- src/qr-stdout/services/qr-stdout.service.spec.ts
+++ src/qr-stdout/services/qr-stdout.service.spec.ts
@@ -1,21 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MockEnvConfigProvider } from '../../common/services/__mocks__';
-import { AfterApplicationBootstrapService } from './after-application-bootstrap.service';
+import { QrStdoutService } from './qr-stdout.service';
 
-describe(`UT:${AfterApplicationBootstrapService.name}`, () => {
+describe(`UT:${QrStdoutService.name}`, () => {
   const enum should {
     createInstance = 'Should create an instance of AfterApplicationBootstrapService properly.',
     callOnApplicationBootstrap = 'Should call onApplicationBootstrap properly.',
   }
 
-  let service: AfterApplicationBootstrapService = null;
+  let service: QrStdoutService = null;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MockEnvConfigProvider, AfterApplicationBootstrapService],
+      providers: [MockEnvConfigProvider, QrStdoutService],
     }).compile();
 
-    service = module.get(AfterApplicationBootstrapService);
+    service = module.get(QrStdoutService);
   });
 
   afterAll(() => {
@@ -24,7 +24,7 @@ describe(`UT:${AfterApplicationBootstrapService.name}`, () => {
 
   it(should.createInstance, () => {
     expect(service).not.toBeNull();
-    expect(service).toBeInstanceOf(AfterApplicationBootstrapService);
+    expect(service).toBeInstanceOf(QrStdoutService);
   });
 
   it(should.callOnApplicationBootstrap, async () => {
