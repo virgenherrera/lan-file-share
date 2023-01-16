e6498bd7b4bc68bdb17c7962ca50681fa11c9ace
Hugo Virgen <virgenherrera@gmail.com>
2023-01-16 10:43:33 -0600

    task: Bump dependencies.
    * drop node v16 engine support.
    * drop node v16 matrix exec in ci.yml.
    * update devDependenciies prettier and rimraf.
    * implement breaking changes for prettier and rimraf.
    * run ncu -u --doctor --enginesNode command.

--- src/upload/interfaces/index.ts
+++ src/upload/interfaces/index.ts
@@ -4,4 +4,4 @@ export type MulterFile = Express.Multer.File;
 export type FileWithDestinationPath = MulterFile & { destinationPath: string };
 export type MimeTypeDataRow = [string, string, string];
 export type MimeTypeSource = MimeTypeDataRow[];
-export type MimeKind = typeof MimeKinds[number];
+export type MimeKind = (typeof MimeKinds)[number];
