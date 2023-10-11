c92c15ba421d5b6fadb84bd8fe7a230d7b88a9db
Hugo Virgen <virgenherrera@gmail.com>
2023-10-11 12:42:55 -0600

    On branch feat/refactor Your branch is ahead of 'origin/feat/refactor' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   test/features/core/get-health.e2e-spec.ts
    	deleted:    test/features/upload/get-mime-types.e2e-spec.ts
    	modified:   test/matchers/index.ts
    
    Changes not staged for commit:
    	modified:   test/features/shared-folder/get-file.e2e-spec.ts

--- test/matchers/index.ts
+++ test/matchers/index.ts
@@ -1,4 +1,3 @@
 export * from './core/get-health.matcher';
-export * from './multimedia/mime-types.matcher';
 export * from './shared-folder/folder-info.matcher';
 export * from './shared-folder/get-downloadable-file-headers';
